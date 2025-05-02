5b274c240c98b8e97d862e9438b65fb84d469cb1
Omars110 <omarsotomayordeavila@gmail.com>
2025-05-01 23:51:06 -0500

    Se limpia el archivo views del modulo contenido

--- expresate/settings.py
+++ expresate/settings.py
@@ -87,7 +87,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'contenidos.middleware.AuthRequiredMiddleware',
     #config for django reload, cors headers : Jhon Alexander
     "django_browser_reload.middleware.BrowserReloadMiddleware",
     'corsheaders.middleware.CorsMiddleware',
